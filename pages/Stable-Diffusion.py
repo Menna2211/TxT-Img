2469e6713c27a50fc6cade1fbd619954e4fda484
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-29 20:43:37 +0300

    Update Stable-Diffusion.py

--- pages/Stable-Diffusion.py
+++ pages/Stable-Diffusion.py
@@ -3,30 +3,57 @@ import torch
 import time
 from diffusers import StableDiffusionPipeline
 
-#device = "cuda" if torch.cuda.is_available() else "cpu"
-#torch_dtype = torch.float16 if device == "cuda" else torch.float32
-#torch_dtype=torch_dtype
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
-#pipe = pipe.to(device)
+
+
+pipe1 = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+
+pipe2 = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
+
 
 st.title("Stable Diffusion App")
 # define the layout of your app
 
 # Define the Streamlit app layout
 prompt = st.text_input("Write your sentence:")
+
+model = st.selectbox("Select a Model", ["Select a Model","Hugging-Face", "Github"])
 submit_button = st.button("Compute")
-if not submit_button:
-   st.stop()
+
+
+if model == "Select a Model" and not submit_button :
+  st.stop()
+
+elif model == "Select a Model" and  submit_button :
+  st.warning('Warning.....!!,Plz..... Select a Model ', icon="⚠️")
 
 # Display the generated text
-if submit_button:
-    with st.spinner('Wait for it...'):
-        generated_img=pipe(prompt).images[0]
-        time.sleep(0.1)
-        
-    st.write("Generated Image:")
-    st.image(generated_img)
-    time.sleep(5)
-    st.success('Congratulations task is done ', icon="✅")
-    st.balloons()
+
+if model == "Hugging-Face":
+  progress_text = "Operation in progress. Please wait."
+  bar = st.progress(0, text=progress_text)
+  for percent_complete in range(100):
+      generated_img=pipe1(prompt).images[0]
+      time.sleep(0.1)
+      bar.progress(percent_complete + 1, text=progress_text)
+
+  # Display the uploaded image and its generated caption
+  st.write("Generated Image:")
+  st.image(generated_img)
+  time.sleep(3)
+  st.success('Congratulations task is done ', icon="✅")
+  st.balloons()
+
+elif model == "Github":
+  progress_text = "Operation in progress. Please wait."
+  bar = st.progress(0, text=progress_text)
+  for percent_complete in range(100):
+      generated_img=pipe2(prompt).images[0]
+      time.sleep(0.1)
+      bar.progress(percent_complete + 1, text=progress_text)
+
+  # Display the uploaded image and its generated caption
+  st.write("Generated Image:")
+  st.image(generated_img)
+  time.sleep(3)
+  st.success('Congratulations task is done ', icon="✅")
+  st.balloons()
