4dab74afac8bdf62634e54af469e79859977431d
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-29 17:01:54 +0300

    Update Stable-Diffusion-Huggingface.py

--- pages/Stable-Diffusion-Huggingface.py
+++ pages/Stable-Diffusion-Huggingface.py
@@ -3,18 +3,19 @@ import torch
 import time
 from diffusers import StableDiffusionPipeline
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if device == "cuda" else torch.float32
+#device = "cuda" if torch.cuda.is_available() else "cpu"
+#torch_dtype = torch.float16 if device == "cuda" else torch.float32
 
 model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch_dtype)
-pipe = pipe.to(device)
+pipe = StableDiffusionPipeline.from_pretrained(model_id)
+#pipe = pipe.to(device)
 
 st.title("Stable Diffusion App")
 # define the layout of your app
 
 # Define the Streamlit app layout
 prompt = st.text_input("Write your sentence:")
+
 submit_button = st.button("Compute")
 if not submit_button:
    st.stop()
