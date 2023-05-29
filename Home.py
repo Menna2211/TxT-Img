86d817d4fe84ca0e36c2fd228ce942a9bf05fd5b
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-29 14:57:48 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,18 +5,18 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.title("Welcome to My TXImg! 👋")
+st.title("Welcome to My TxT-Img! 👋")
 
 st.markdown(
     """
-    TXImg is an open-source app built specifically for Image Captioning and Stable Diffusion projects.
+    TxT-Img is an open-source app built specifically for Image Captioning and Stable Diffusion projects.
     ### Image Caption:
     The application allows users to upload an image and generate a descriptive caption for the image Using:
     - Hugging Face Model: [blip-image-captioning-base](https://huggingface.co/Salesforce/blip-image-captioning-base)
     - Github model: [CATR](https://github.com/saahiluppal/catr)
     ### Stable Diffusion:
     The application allows users to input a piece of text and generate an image that is related to the input text. 
-    - Hugging Face Model: [openjourney](https://huggingface.co/prompthero/openjourney?text=man+riding+horse)
+    - Hugging Face Model: [stable-diffusion-v1-5](https://huggingface.co/runwayml/stable-diffusion-v1-5)
     - Github model: [github](https://github.com)
 """
 )
